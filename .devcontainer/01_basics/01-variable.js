c3889563b56e6e839c70eec00f254688408032ee
Chiranjibi Rout <190082840+ChiranjibiR@users.noreply.github.com>
2025-05-12 15:53:24 +0000

    initial commits

---
+++ .devcontainer/01_basics/01-variable.js
@@ -0,0 +1,7 @@
+const accountId=3452;
+let accountEmail="rout@gmail.com";
+var accountPassword="1234";
+accountCity="Bhubaneswar";
+let accountState;
+
+console.table([accountId = accountId,accountEmail,accountPassword,accountCity,accountState]);
